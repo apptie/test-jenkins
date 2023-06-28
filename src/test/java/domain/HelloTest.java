6979440e6f9e1e00e089dc50d04d081d749d1fb5
apptie <appti@naver.com>
2023-06-28 13:59:37 +0900

    commit

--- src/test/java/domain/HelloTest.java
+++ src/test/java/domain/HelloTest.java
@@ -1,7 +1,26 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class HelloTest {
 
+    Hello hello;
+
+    @BeforeEach
+    void setUp() {
+        hello = new Hello();
+    }
+
+    @Test
+    void hello() {
+        assertEquals(hello.hello(), "hello");
+    }
+
+    @Test
+    void hi() {
+        assertEquals(hello.hi(), "hi");
+    }
 }
